d336e0556270f3270e6cbd04e26d47563ca91963
dladerriere <dladerriere@gmail.com>
2023-02-03 09:58:53 +0100

    [feat] implemented loot, users and characters in server

--- server.js
+++ server.js
@@ -6,15 +6,22 @@ const cors = require('cors');
  
 const PORT = process.env.PORT || 3001 ;
 const landRoutes = require('./routes/land');
+const userRoutes = require('./routes/user');
+const charRoutes = require('./routes/char');
+const lootRoutes = require('./routes/loot');
  
 // database connection
 require('./config/database.js');
  
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
  
 // routes
 app.use('/api/land', landRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/char', charRoutes);
+app.use('/api/loot', lootRoutes);
  
 // server running status
 app.listen(PORT, () => {
